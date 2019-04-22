3d2c6c630ea6484d9a1a6acddd22253a2f01d244
corysbutler <cory.butler@ymail.com>
2019-04-22 12:28:52 -0500

    Validate a List<List<byte>> grid

---
+++ Sudoku/Checker.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sudoku
+{
+    public static class Checker
+    {
+        public static bool IsValidGrid(List<List<byte>> grid)
+        {
+            return AreAllBlocksValid(grid) && AreAllColumnsValid(grid) && AreAllRowsValid(grid);
+        }
+
+        private static bool IsValidCell(byte cell) => cell > 0 && cell < 10;
+
+        private static bool AreAllBlocksValid(List<List<byte>> grid)
+        {
+            var blockWidth = 3;
+            var blockHeight = 3;
+            var rowBlocks = 3;
+            var columnBlocks = 3;
+
+            for (var blockX = 0; blockX < rowBlocks; blockX++)
+            {
+                for (var blockY = 0; blockY < columnBlocks; blockY++)
+                {
+                    for (var x = 0; x < blockWidth; x++)
+                    {
+                        var foundValues = new List<byte>();
+
+                        for (var y = 0; y < blockHeight; y++)
+                        {
+                            var cell = grid[x + blockX * blockWidth][y + blockY * blockHeight];
+                            if (!IsValidCell(cell) || foundValues.Any(v => v == cell)) return false;
+                            foundValues.Add(cell);
+                        }
+                    }
+                }
+            }
+            return true;
+        }
+
+        private static bool AreAllColumnsValid(List<List<byte>> grid)
+        {
+            for (var x = 0; x < grid.Count; x++)
+            {
+                var foundValues = new List<byte>();
+
+                for (var y = 0; y < grid[x].Count; y++)
+                {
+                    var cell = grid[y][x];
+                    if (!IsValidCell(cell) || foundValues.Any(v => v == cell)) return false;
+                    foundValues.Add(cell);
+                }
+            }
+            return true;
+        }
+
+        private static bool AreAllRowsValid(List<List<byte>> grid)
+        {
+            for (var x = 0; x < grid.Count; x++)
+            {
+                var foundValues = new List<byte>();
+
+                for (var y = 0; y < grid[x].Count; y++)
+                {
+                    var cell = grid[x][y];
+                    if (!IsValidCell(cell) || foundValues.Any(v => v == cell)) return false;
+                    foundValues.Add(cell);
+                }
+            }
+            return true;
+        }
+    }
+}
--- Sudoku/Program.cs
+++ Sudoku/Program.cs
@@ -10,6 +10,20 @@ namespace Sudoku
     {
         static void Main(string[] args)
         {
+            List<List<byte>> grid = new List<List<byte>>() {
+                new List<byte>() { 5, 3, 4, 6, 7, 8, 9, 1, 2 },
+                new List<byte>() { 6, 7, 2, 1, 9, 5, 3, 4, 8 },
+                new List<byte>() { 1, 9, 8, 3, 4, 2, 5, 6, 7 },
+                new List<byte>() { 8, 5, 9, 7, 6, 1, 4, 2, 3 },
+                new List<byte>() { 4, 2, 6, 8, 5, 3, 7, 9, 1 },
+                new List<byte>() { 7, 1, 3, 9, 2, 4, 8, 5, 6 },
+                new List<byte>() { 9, 6, 1, 5, 3, 7, 2, 8, 4 },
+                new List<byte>() { 2, 8, 7, 4, 1, 9, 6, 3, 5 },
+                new List<byte>() { 3, 4, 5, 2, 8, 6, 1, 7, 9 }
+            };
+
+            Console.WriteLine(Checker.IsValidGrid(grid));
+            Console.ReadLine();
         }
     }
 }
